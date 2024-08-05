8fe18981a34228fe55f643445117b697828928a7
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2024-08-05 15:09:25 +0200

    Initial Commit

---
+++ MotivationApp/MotivationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationAppApp.swift
+//  MotivationApp
+//
+//  Created by Tako Menteshashvili on 05.08.24.
+//
+
+import SwiftUI
+
+@main
+struct MotivationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
